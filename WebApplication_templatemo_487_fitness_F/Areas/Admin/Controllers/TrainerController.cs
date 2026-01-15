d5b03b2a3153b8895338665c11601e68706f708b
7Ferid <faridgg-mpa101@code.edu.az>
2026-01-15 13:54:28 +0400

    Crud1/2

---
+++ WebApplication_templatemo_487_fitness_F/Areas/Admin/Controllers/TrainerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication_templatemo_487_fitness_F.Contexts;
+using WebApplication_templatemo_487_fitness_F.ViewModels.TrainerViewModels;
+
+namespace WebApplication_templatemo_487_fitness_F.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TrainerController(AppDbContext _context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var trainers = await _context.Trainers.Select(x => new TrainerGetVM()
+            {
+              Id=x.Id,
+                Name=x.Name,
+                ImagePath=x.ImagePath,
+                Duties=x.Duties,
+                DepartmentName = x.Department.Name
+
+            }).ToListAsync();
+            return View(trainers);
+
+            
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var trainers = await _context.Trainers.ToListAsync();
+            ViewBag.Trainers = trainers;
+
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication_templatemo_487_fitness_F/ViewModels/TrainerViewModels/TrainerGetVM.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication_templatemo_487_fitness_F.ViewModels.TrainerViewModels
+{
+    public class TrainerGetVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public string Duties { get; set; } = string.Empty;
+
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+
+}
